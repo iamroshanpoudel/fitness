8619c4ca0291afe8d9a3500605daf90a7b0a92e1
Roshan Poudel <roshan@Roshans-Macbook.local>
2021-03-27 00:23:56 +0900

    completed navbar component

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from './images/image.jpeg';
+
+
+const Nav = (props) => {
+    return (
+    <div id="nav">
+      <h2 className="logo">Day Logger</h2>
+      <div id="links">
+        <a href="#" id="active-link">Log Day</a>
+        <a href="#">Edit Questions</a>
+        <a href="#">View Data</a>
+      </div>    
+        <img src={Image} id="image" alt="Logger"/>
+    </div>
+    );
+
+}
+
+
+export default Nav;
