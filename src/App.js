f80854e99e47ba7b84a049b3ce9036471d4cada0
Roshan Poudel <roshan@Roshans-Macbook.local>
2021-03-27 17:33:42 +0900

    completed log page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import './App.css';
-import Nav from './Nav';
-import Calendar from './Calendar';
-import React, { useState } from 'react';
+import "./App.css";
+import Nav from "./Nav";
+import Calendar from "./Calendar";
+import FormRow from "./FormRow";
+import React, { useState } from "react";
 
 function App() {
-  const [questionState, setQuestionState] = useState(
-    [
-      { type: "number", question: "Number of pushups", answer: "", options: [] },
-      { type: "boolean", question: "Had a long walk today", answer: "", options: ["True", "False"] },
-      { type: "text", question: "One great thing that happened today", answer: "", options: [] },
-      { type: "multiple-choice", question: "Today was a:", answer: "", options: ["Ok day", "Bad day", "Great day"] }
-    ]
-  );
-  return (
-    <div className="App">
-      <Nav />
-      <Calendar />
-    </div>
-  );
+	const [questionState, setQuestionState] = useState([
+		{ type: "number", question: "Number of pushups", answer: "", options: [] },
+		{
+			type: "boolean",
+			question: "Had a long walk today",
+			answer: "",
+			options: ["True", "False"],
+		},
+		{
+			type: "text",
+			question: "One great thing that happened today",
+			answer: "",
+			options: [],
+		},
+		{
+			type: "multiple-choice",
+			question: "Today was a:",
+			answer: "",
+			options: ["Ok day", "Bad day", "Great day"],
+		},
+	]);
+	return (
+		<div className="App">
+			<Nav />
+			<div id="body-items">
+				<Calendar />
+				<form action="" method="POST">
+					<div id="form-section">
+						{questionState.map((question) => {
+							return <FormRow question={question} />;
+						})}
+					</div>
+					<div>
+						<input type="submit" value="Save" id="save-button"></input>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/FormRow.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const FormRow = (props) => {
+	// Returns JSX with appropriate input options according to type
+	const getInputFormat = () => {
+		if (props.question.type === "text") {
+			return <input type="text"></input>;
+		} else if (props.question.type === "boolean") {
+			return (
+				<div className="boolean-selectors">
+					<div>
+						<input type="radio" value="True" name="radio-btn" />
+						True
+					</div>
+					<div>
+						<input type="radio" value="False" name="radio-btn" />
+						False
+					</div>
+				</div>
+			);
+		} else if (props.question.type === "number") {
+			return <input type="number"></input>;
+		} else {
+			return (
+				<div className="multiple-selectors">
+					{props.question.options.map((option) => {
+						return (
+							<div>
+								<input type="radio" value={option} name="radio-btn" />
+								{option}
+							</div>
+						);
+					})}
+				</div>
+			);
+		}
+	};
+	return (
+		<div className="form-row">
+			<div>{props.question.question}</div>
+			<div>{getInputFormat()}</div>
+		</div>
+	);
+};
+
+export default FormRow;
