0a499017ae2c4da1ca5e466719ee60b7f6d3a292
Singularity2050 <67400401+Singularity2050@users.noreply.github.com>
2021-05-27 22:51:55 +0900

    Header Design
    Header Design

---
+++ src/Components/Nav/HeaderLinks.js
@@ -0,0 +1,119 @@
+/*eslint-disable*/
+import React, {useState} from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import {Link, NavLink} from "react-router-dom";
+import {GoogleLogin, useGoogleLogout }from 'react-google-login';
+import { responseFailGoogle,logout} from "../../util/googleLogin";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+import logo from '../../images/logo.png'
+// core components
+import CustomDropdown from "./CustomDropdown.js";
+import Button from "./Button.js";
+
+import styles from "../../util/headerLinkStyle";
+import headerStyle from "../../util/headerStyle";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+
+
+    const defaultImage =
+        "https://res.cloudinary.com/roshanpoudel/image/upload/v1620734424/userProfileImages/defaultImage.svg";
+
+
+    ///////////////////////////////////////Google Login Function////////////////////////////////////////////////
+    const [image, setImage] = useState();
+    //sign out hook
+    const { signOut, signOutLoaded } = useGoogleLogout({
+        clientId:"547391741830-p8ru0i3urt5bhnt5nqief36ns3n20gqv.apps.googleusercontent.com",
+        buttonText:"Logout",
+        Style:"display:none",
+        className:"logout",
+        onLogoutSuccess:{logout}
+    })
+    //signIn response
+    const responseGoogle = (response) => {
+        document.getElementById('googleLogin').style= 'display:none'
+        document.getElementById('googleHide').style ='display:block'
+        setImage(response.profileObj.imageUrl);
+        //Timing to renew access token
+        let expired_at = 24 * 60 * 1000; //One Day
+        //add expiration information
+        response.profileObj.expired_at = expired_at;
+        //store in session Storage
+        sessionStorage.setItem('userData', JSON.stringify(response.profileObj))
+        const timeOut = async () =>{
+            const sessionClear = () =>sessionStorage.removeItem('userData');
+            await setTimeout(sessionClear,expired_at);
+            await setTimeout(signOut,expired_at);
+        }
+        //start Counting
+        timeOut().then(r =>{ console.log("session started")});
+    }
+    console.log(headerStyle.primary)
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    const classes = useStyles();
+    return (
+        <>
+        <List className={classes.list}>
+            <ListItem className={classes.listItem}>
+                <NavLink to="/main" activeClassName="active-link" className="logo alink"><img id='logo' src={logo}/></NavLink>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                <Button
+                    href="/log"
+                    color="transparent"
+                    className={classes.navLink}
+                >Log Day
+                </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+                    <Button
+                        href="./questions"
+                        color="transparent"
+                        className={classes.navLink}
+                    >Edit Questions
+                    </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+
+                    <Button
+                        color="transparent"
+                        href="/view"
+                        className={classes.navLink}
+                    >View Data
+                    </Button>
+            </ListItem>
+        </List>
+    <div id='googleLogin'>
+        <GoogleLogin
+            clientId="547391741830-p8ru0i3urt5bhnt5nqief36ns3n20gqv.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseFailGoogle}
+            cookiePolicy={'single_host_origin'}
+            className='login'
+            isSignedIn={true}
+        />
+        <p id='failure'></p>
+    </div>
+    <div id='googleHide'>
+        {image === undefined ? (
+            <a href='./profile'><img src={defaultImage} id="image" alt="User" /></a>
+        ) : (
+            <a href='./profile'><img src={image} id="image" alt="User" /></a>
+        )}
+    </div>
+    </>
+    );
+}
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,99 +1,12 @@
 import React,{ useState } from "react";
-import { NavLink } from "react-router-dom";
-import {GoogleLogin, useGoogleLogout }from 'react-google-login';
-import { responseFailGoogle,logout} from "../../util/googleLogin";
-import logo from '../../images/logo.png'
+import Header from "./Header";
+import headerStyle from "../../util/headerStyle";
+import HeaderLinks from "./HeaderLinks";
 
 const Nav = () => {
 
-	const headerColor = () =>{
-		if(window.location.pathname == '/cse316/main' || window.location.pathname == '/main'){
-			return 'navMain'
-		}else{
-			return 'nav'
-		}
-	}
-
-	const defaultImage =
-		"https://res.cloudinary.com/roshanpoudel/image/upload/v1620734424/userProfileImages/defaultImage.svg";
-
-	///////////////////////////////////////Google Login Function////////////////////////////////////////////////
-	const [image, setImage] = useState();
-	//sign out hook
-	const { signOut, signOutLoaded } = useGoogleLogout({
-		clientId:"547391741830-p8ru0i3urt5bhnt5nqief36ns3n20gqv.apps.googleusercontent.com",
-		buttonText:"Logout",
-		Style:"display:none",
-		className:"logout",
-		onLogoutSuccess:{logout}
-	})
-	//signIn response
-	const responseGoogle = (response) => {
-		document.getElementById('googleLogin').style= 'display:none'
-		document.getElementById('googleHide').style ='display:block'
-		setImage(response.profileObj.imageUrl);
-		//Timing to renew access token
-		let expired_at = 24 * 60 * 1000; //One Day
-		//add expiration information
-		response.profileObj.expired_at = expired_at;
-		//store in session Storage
-		sessionStorage.setItem('userData', JSON.stringify(response.profileObj))
-		const timeOut = async () =>{
-			const sessionClear = () =>sessionStorage.removeItem('userData');
-			await setTimeout(sessionClear,expired_at);
-			await setTimeout(signOut,expired_at);
-		}
-		//start Counting
-		timeOut().then(r =>{ console.log("session started")});
-	}
-
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////
 	return (
-		<div id={headerColor()}>
-			<div id="links">
-				<NavLink to="/main" activeClassName="active-link" className="logo alink"><img id='logo' src={logo}/></NavLink>
-				<NavLink to="/log" activeClassName="active-link" className="alink" exact>
-					Log Day
-				</NavLink>
-				<NavLink
-					to="questions"
-					activeClassName="active-link"
-					className="alink"
-					exact
-				>
-					Edit Questions
-				</NavLink>
-				<NavLink
-					to="/view"
-					activeClassName="active-link link"
-					className="alink"
-					exact
-				>
-					View Data
-				</NavLink>
-				<div id='googleLogin'>
-					<GoogleLogin
-						clientId="547391741830-p8ru0i3urt5bhnt5nqief36ns3n20gqv.apps.googleusercontent.com"
-						buttonText="Login"
-						onSuccess={responseGoogle}
-						onFailure={responseFailGoogle}
-						cookiePolicy={'single_host_origin'}
-						className='login'
-						isSignedIn={true}
-					/>
-				</div>
-				<p id='failure'></p>
-				<div id='googleHide'>
-				<NavLink to="/profile" exact>
-					{image === undefined ? (
-						<img src={defaultImage} id="image" alt="User" />
-					) : (
-						<img src={image} id="image" alt="User" />
-					)}
-				</NavLink>
-				</div>
-			</div>
-		</div>
+		<Header props={headerStyle.primary}/>
 	);
 };
 
