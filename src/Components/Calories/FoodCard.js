2b903fb6192cb3b4bafa1c2f28f5d8352abb7baf
Roshan Poudel <roshan.poudel@stonybrook.edu>
2021-06-06 11:37:48 +0900

    added unsplash images and fixed null food bug

--- src/Components/Calories/FoodCard.js
+++ src/Components/Calories/FoodCard.js
@@ -78,7 +78,7 @@ const FoodCard = (props) => {
 			/>
 			<CardMedia
 				className={classes.media}
-				image="https://images.unsplash.com/photo-1575159240102-4331f59433ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+				image={props.food.image}
 				title={props.food.foodName}
 			/>
 
--- src/Components/Calories/LogCalories.js
+++ src/Components/Calories/LogCalories.js
@@ -26,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
 		strokeWidth: 1,
 	},
 }));
+
 const LogCalories = (props) => {
 	// Returns current date as string
 	const addDate = () => {
